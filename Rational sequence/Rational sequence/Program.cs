fa0fc32a5383345a22fffce7dc782ab6531280b7
JonasManley <54889189+JonasManley@users.noreply.github.com>
2019-10-08 08:58:45 +0200

    Project added

---
+++ Rational sequence/Rational sequence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rationals rationals = new Rationals();
+            Console.WriteLine(rationals.GetRationalNumber(15));
+
+            Console.WriteLine("----------------");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
